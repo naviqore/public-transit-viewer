55ea8059d9a77b79c690d740f3c3f39aaa91578f
Merlin Unterfinger <info@munterfinger.ch>
2024-08-22 01:12:01 +0200

    ORG: NAV-150 - Add dockerfile for building docker image

--- public_transit_viewer/entrypoint.py
+++ public_transit_viewer/entrypoint.py
@@ -10,7 +10,8 @@ def main():
     main_py_path = os.path.join(script_dir, "public_transit_viewer", "main.py")
 
     subprocess.run(
-        ["streamlit", "run", main_py_path, f"--server.port={SERVER_PORT}", f"--server.address={SERVER_ADDRESS}"],
+        ["streamlit", "run", main_py_path, f"--server.port={SERVER_PORT}", f"--server.address={SERVER_ADDRESS}",
+         "--browser.gatherUsageStats=false"],
         cwd=script_dir
     )
 
