8dba7333b8e8b2f4126597e3a9fdf3901a72b100
Lukas Connolly <lukas_connolly@hotmail.com>
2024-09-25 22:56:06 +0200

    STYLE: Replace transparent with round logo.

--- public_transit_viewer/__init__.py
+++ public_transit_viewer/__init__.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 
-LOGO_PATH = Path(__file__).parent / "assets" / "naviqore-transparent.png"
+LOGO_PATH = Path(__file__).parent / "assets" / "naviqore-round.png"
 ICON_PATH = Path(__file__).parent / "assets" / "favicon.ico"
